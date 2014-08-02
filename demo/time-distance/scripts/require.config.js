ec50b1ceaae9815ec468b48dc93c594afe0d677c
janus_wel <janus.wel.3@gmail.com>
2014-08-02 16:51:08 +0900

    IE friendly

--- demo/time-distance/scripts/require.config.js
+++ demo/time-distance/scripts/require.config.js
@@ -5,10 +5,10 @@ var require = {
     baseUrl: './scripts',
     paths: {
         jquery: 'http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min',
-        'jQuery.UI': 'http://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min',
+        'jQuery.UI': 'http://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min'
     },
     shim: {
-        'jQuery.UI': ['jquery'],
+        'jQuery.UI': ['jquery']
     }
 };
 
